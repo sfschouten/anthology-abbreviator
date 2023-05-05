6cb4db1c615fc558723136b8c22381c41c82456a
Stefan F. Schouten <sfschouten@gmail.com>
2023-05-05 16:47:47 +0200

    subtree

---
+++ update_subtrees.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# add dataset repos as remotes
+git remote add -f acl_anthology_repo https://github.com/acl-org/acl-anthology.git
+
+# pull the latest version of the repos
+git subtree pull --prefix acl-anthology/ acl_anthology_repo main --squash
+
