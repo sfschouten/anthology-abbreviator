fc2a2c70ce37e996811ed785f0c0c95558c57e1a
Stefan F. Schouten <sfschouten@gmail.com>
2023-05-05 17:05:16 +0200

    acl-anthology has master not main

--- update_subtrees.sh
+++ update_subtrees.sh
@@ -4,5 +4,5 @@
 git remote add -f acl_anthology_repo https://github.com/acl-org/acl-anthology.git
 
 # pull the latest version of the repos
-git subtree pull --prefix acl-anthology/ acl_anthology_repo main --squash
+git subtree pull --prefix acl-anthology/ acl_anthology_repo master --squash
 
